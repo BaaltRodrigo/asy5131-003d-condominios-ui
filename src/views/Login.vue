ca56e485e40a8c8221fdd21ee59cf25c3e6fbbc7
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-05-22 18:44:34 -0400

    refactor: use of auth functions in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,13 +5,15 @@
         <v-card>
           <v-card-title class="justify-center"> Titulo Bonito </v-card-title>
           <v-card-text>
-            <v-form refs="form">
+            <v-form ref="form">
               <v-text-field
+                v-model="email"
                 outlined
                 label="Correo"
                 :rules="[rules.required]"
               ></v-text-field>
               <v-text-field
+                v-model="password"
                 outlined
                 :type="showPassword ? 'text' : 'password'"
                 label="Password"
@@ -20,7 +22,9 @@
                 :rules="[rules.required]"
               ></v-text-field>
             </v-form>
-            <v-btn block depressed color="primary">Ingresar</v-btn>
+            <v-btn block depressed color="primary" @click="sendLogin"
+              >Ingresar</v-btn
+            >
           </v-card-text>
           <v-divider></v-divider>
           <v-card-text> Opciones extras </v-card-text>
@@ -31,16 +35,47 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "Login",
 
   data() {
     return {
+      email: null,
+      password: null,
       showPassword: false,
       rules: {
         required: (value) => !!value || "Este campo es obligatorio!",
       },
     };
+  },
+
+  methods: {
+    ...mapActions("auth", ["login", "checkToken"]),
+    async sendLogin() {
+      if (!this.$refs.form.validate()) return;
+      // Enviar request
+      const isLogged = await this.login({
+        email: this.email,
+        password: this.password,
+      });
+      // Si trae cosas malas, alerta
+      if (!isLogged) {
+        this.showAlert = true;
+        return;
+      }
+      // Trae cosas buenas, redirect
+      this.$router.push({ name: "Home" });
+    },
+  },
+
+  async created() {
+    // Check if access token is valid
+    const hasValidToken = await this.checkToken();
+    if (hasValidToken) {
+      this.$router.push({ name: "Home" });
+    }
   },
 };
 </script>
