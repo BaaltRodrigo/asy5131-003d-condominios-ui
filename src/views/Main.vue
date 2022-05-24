c63ee30df517bc4650a36999967ab65a302065e9
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-05-23 22:45:22 -0400

    fix: :bug: Check for token when initialize app

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,39 @@
 <template>
   <v-app>
+    <the-header></the-header>
+    <the-sidebar></the-sidebar>
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script>
+import { mapActions } from "vuex";
+import TheHeader from "../components/general/TheHeader.vue";
+import TheSidebar from "../components/general/TheSidebar.vue";
+export default {
+  name: "Main",
+
+  components: {
+    TheHeader,
+    TheSidebar,
+  },
+
+  methods: {
+    ...mapActions("auth", ["checkToken"]),
+  },
+
+  async created() {
+    if (!localStorage.access_token) {
+      return this.$router.push({ name: "Login" });
+    }
+
+    const isValid = await this.checkToken();
+    if (!isValid) {
+      localStorage.clear("access_token");
+      return this.$router.push({ name: "Login" });
+    }
+  },
+};
+</script>
