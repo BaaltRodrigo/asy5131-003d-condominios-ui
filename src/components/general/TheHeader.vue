fe2c75d882d70ddf1cabd9ef2b03745af7cb26b8
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-05-23 22:46:08 -0400

    feat: Basic layout

---
+++ src/components/general/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app clipped-left>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
---
+++ src/components/general/TheSidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-navigation-drawer app clipped permanent>
+    aca poner cosas de sidebar
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "TheSidebar",
+};
+</script>
